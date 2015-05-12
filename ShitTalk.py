0c18226f9dc17d84cc5f4bb12fb36a98697964b9
zaxguo <zaxguo@me.com>
2015-05-12 16:03:45 +0800

    Server constructed and basic page layout drafted

--- ShitTalk.py
+++ ShitTalk.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+    
