de72091bf77940203b7781a17e37618bda690a8f
zaxguo <zaxguo@me.com>
2015-06-13 19:08:37 +0800

    new file:   gunicorn.conf 	new file:   nginx.conf

--- ShitTalk.py
+++ ShitTalk.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
 from Model import Post
+from werkzeug.contrib.fixers import ProxyFix
 import os
 import sys
 
@@ -14,14 +14,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
     
